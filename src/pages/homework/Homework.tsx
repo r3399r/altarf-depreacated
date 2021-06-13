3fc04f73f7b089480165e90261b075ec75fb4300
Yue Chang <r3399r@gmail.com>
2021-06-13 19:36:30 +0800

    test version

--- src/pages/homework/Homework.tsx
+++ src/pages/homework/Homework.tsx
@@ -5,7 +5,7 @@ const Homework = () => {
   const { Panel } = Collapse;
 
   const text = `
-  A dog is a type of domesticated animal.
+  A dog is a kind of domesticated animal.
   Known for its loyalty and faithfulness,
   it can be found as a welcome guest in many households across the world.
 `;
